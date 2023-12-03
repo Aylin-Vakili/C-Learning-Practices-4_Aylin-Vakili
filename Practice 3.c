8a33065b9021b77d407be3b77a7c49321a21104a
Aylin-Vakili <150549117+Aylin-Vakili@users.noreply.github.com>
2023-12-03 17:09:19 +0330

    Update Practice 3.c

--- Practice 3.c
+++ Practice 3.c
@@ -10,9 +10,18 @@ int main()
   printf("\n Enter one of characters +, -, *, / with two integers: ");
   scanf("%c %d %d",&c ,&a ,&b);
 
-  // displays output
-  printf("\n %c %d %d\n",c ,a ,b);
+ if( c==+ ) { a+=b 
+   printf( "%d",a ) }
+  
+  if( c==- ) { a-=b 
+   printf( "%d",a ) }
 
+  if( c==* ) { a*=b 
+   printf( "%d",a ) }
+
+  if( c==/ ) { a/=b 
+   printf( "%d",a ) }
+  
   return 0;
 }
 //Made by Aylin Vakili - Student number 14025141115 - Practice 3
